7f4d2f5bb19855b635efe0c31fe4758c044c06fb
miller <millershaw61@gmail.com>
2023-12-07 17:02:33 +0000

    refining

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,18 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { title } from 'process'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <html>
-      <title>
+    <div>
+      <head>
+        <title>
+          nea project
+        </title>
+      </head>
+      <header>
         nea project
-      </title>
-      <body>
-        <div>
-          <header>
-            nea project
-          </header>
-        </div>
-      </body>
-    </html>
+      </header>
+    </div>
   )
 }
