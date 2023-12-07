7fdda544d024fbc06f5afdc660b9326d50680478
miller <millershaw61@gmail.com>
2023-12-07 11:57:37 +0000

    ammended dbts

--- db.ts
+++ db.ts
@@ -1,2 +1,6 @@
-import { sqlite3 } from "sqlite3";
-const dbPath = ''
+import path from "path";
+import sqlite3, { Database } from "sqlite3";
+const dbPath = path.resolve(__dirname, 'data', 'database.db')
+
+const db = new sqlite3.Database(dbPath);
+export default db;
