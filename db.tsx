399933b124b7971e644b281437f0cd2341f7475a
miller <millershaw61@gmail.com>
2023-12-08 10:33:33 +0000

    edits

--- db.tsx
+++ db.tsx
@@ -1,5 +1,5 @@
 import path from "path";
-import sqlite3, { Database } from "sqlite3";
+import sqlite3 , { Database } from "sqlite3";
 const dbPath = path.resolve(__dirname, 'data', 'database.db')
 
 const db = new sqlite3.Database(dbPath);
