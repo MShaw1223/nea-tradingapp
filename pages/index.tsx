eb18ece72c760e5dd81897a1020d66980603d74e
miller <millershaw61@gmail.com>
2023-12-08 10:15:16 +0000

    comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-
+//change the option values to the actual things
 export default function Home() {
   return (
-    <div>
-      <head>
-        <title>
+    <div style={{display: 'flex', }}>
+      <div style={{marginRight:'20px', marginTop:'200px'}}>
+        <select>
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      </div>
+      <div style={{textAlign: 'center', fontSize:'50px', marginTop:'35px'}}>
+        <head>
+          <title>
+            nea project
+          </title>
+        </head>
+        <header>
           nea project
-        </title>
-      </head>
-      <header>
-        nea project
-      </header>
+        </header>
+      </div>
     </div>
   )
 }
