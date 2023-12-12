6addc8bbfeaa6cb937d5c32adfbf6ac58ebee1f4
miller <millershaw61@gmail.com>
2023-12-12 16:51:35 +0000

    small changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,27 @@
 import { ComboboxEntryChoice, ComboboxAccountChoice } from '@/components/ui/combobox'
-import { ButtonOutline } from '@/components/ui/outline'
-import { Input } from '@/components/ui/input'
 import { InputWithButton } from '@/components/ui/withButton'
 
 export default function Home() {
   return (
-      <div>
-          <div className='flex justify-center text-xl font-extrabold p-4 bg-slate-300'>
-            NEA project
+    <div>
+      <div className='flex justify-center text-xl font-extrabold p-4 bg-slate-300'>
+        NEA project
+      </div>
+      <div className="relative p-6">
+        <div className='static flex justify-center'>
+          <div className='p-6'>
+            <ComboboxAccountChoice/>
           </div>
-        <div className="relative p-6">
-          <div className='static flex justify-center'>
-            <div className='p-6'>
-              <ComboboxAccountChoice/>
-            </div>
-            <div className='p-6'>
-              <ComboboxEntryChoice/>
-            </div>
+          <div className='p-6'>
+            <ComboboxEntryChoice/>
           </div>
-          <div className='static flex justify-center'>
-            <div className='p-3'>
-              <InputWithButton/>
-            </div>
+        </div>
+        <div className='static flex justify-center'>
+          <div className='p-3'>
+            <InputWithButton/>
           </div>
         </div>
       </div>
+    </div>
   )
 }
-
